d3dc05adce400e8e682728597ed6f60f6e25448c
HarrySheep <gdhaoyang@hotmail.com>
2018-08-06 16:22:12 +0800

    Site updated: 2018-08-06 16:22:10

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a730013f692af3bb997241e2f490c79c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
